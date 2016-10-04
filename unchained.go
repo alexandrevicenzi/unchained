00e039b139712e5748a5fa0330c8fe6d3ffc1773
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2016-10-04 20:42:18 -0300

    Go fmt.

--- unchained.go
+++ unchained.go
@@ -2,142 +2,142 @@
 package unchained
 
 import (
-    "encoding/base64"
-    "fmt"
-    "crypto/hmac"
-    "crypto/sha1"
-    "crypto/sha256"
-    "golang.org/x/crypto/pbkdf2"
-    "strconv"
-    "strings"
+	"crypto/hmac"
+	"crypto/sha1"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"golang.org/x/crypto/pbkdf2"
+	"strconv"
+	"strings"
 )
 
 func b64encode(bytes []byte) string {
-    return base64.StdEncoding.EncodeToString(bytes)
+	return base64.StdEncoding.EncodeToString(bytes)
 }
 
 func compareDigest(val1, val2 string) bool {
-    return hmac.Equal([]byte(val1), []byte(val2))
+	return hmac.Equal([]byte(val1), []byte(val2))
 }
 
 func hashAlgorithm(encoded string) string {
-    return strings.Split(encoded, "$")[0]
+	return strings.Split(encoded, "$")[0]
 }
 
 // Encode raw password using PBKDF2 SHA256 hasher.
 func EncodePBKDF2SHA256(password string, salt string, iterations int) string {
-    d := pbkdf2.Key([]byte(password), []byte(salt), iterations, sha256.Size, sha256.New)
-    hash := b64encode(d)
-    return fmt.Sprintf("pbkdf2_sha256$%d$%s$%s", iterations, salt, hash)
+	d := pbkdf2.Key([]byte(password), []byte(salt), iterations, sha256.Size, sha256.New)
+	hash := b64encode(d)
+	return fmt.Sprintf("pbkdf2_sha256$%d$%s$%s", iterations, salt, hash)
 }
 
 // Validate raw password using PBKDF2 SHA256 hasher.
 func VerifyPBKDF2SHA256(password string, encoded string) bool {
-    s := strings.Split(encoded, "$")
+	s := strings.Split(encoded, "$")
 
-    if len(s) != 4 {
-        return false
-    }
+	if len(s) != 4 {
+		return false
+	}
 
-    algorithm, iterations, salt := s[0], s[1], s[2]
+	algorithm, iterations, salt := s[0], s[1], s[2]
 
-    if (algorithm != "pbkdf2_sha256") {
-        return false
-    }
+	if algorithm != "pbkdf2_sha256" {
+		return false
+	}
 
-    i, err := strconv.Atoi(iterations)
+	i, err := strconv.Atoi(iterations)
 
-    if (err != nil) {
-        return false
-    }
+	if err != nil {
+		return false
+	}
 
-    newencoded := EncodePBKDF2SHA256(password, salt, i)
-    return compareDigest(newencoded, encoded)
+	newencoded := EncodePBKDF2SHA256(password, salt, i)
+	return compareDigest(newencoded, encoded)
 }
 
 // Encode raw password using PBKDF2 SHA1 hasher.
 func EncodePBKDF2SHA1(password string, salt string, iterations int) string {
-    d := pbkdf2.Key([]byte(password), []byte(salt), iterations, sha1.Size, sha1.New)
-    hash := b64encode(d)
-    return fmt.Sprintf("pbkdf2_sha1$%d$%s$%s", iterations, salt, hash)
+	d := pbkdf2.Key([]byte(password), []byte(salt), iterations, sha1.Size, sha1.New)
+	hash := b64encode(d)
+	return fmt.Sprintf("pbkdf2_sha1$%d$%s$%s", iterations, salt, hash)
 }
 
 // Validate raw password using PBKDF2 SHA1 hasher.
 func VerifyPBKDF2SHA1(password string, encoded string) bool {
-    s := strings.Split(encoded, "$")
+	s := strings.Split(encoded, "$")
 
-    if len(s) != 4 {
-        return false
-    }
+	if len(s) != 4 {
+		return false
+	}
 
-    algorithm, iterations, salt := s[0], s[1], s[2]
+	algorithm, iterations, salt := s[0], s[1], s[2]
 
-    if (algorithm != "pbkdf2_sha1") {
-        return false
-    }
+	if algorithm != "pbkdf2_sha1" {
+		return false
+	}
 
-    i, err := strconv.Atoi(iterations)
+	i, err := strconv.Atoi(iterations)
 
-    if (err != nil) {
-        return false
-    }
+	if err != nil {
+		return false
+	}
 
-    newencoded := EncodePBKDF2SHA1(password, salt, i)
-    return compareDigest(newencoded, encoded)
+	newencoded := EncodePBKDF2SHA1(password, salt, i)
+	return compareDigest(newencoded, encoded)
 }
 
 // IsPasswordUsable returns true if encoded password is usable.
 func IsPasswordUsable(encoded string) bool {
-    if (strings.HasPrefix("!", encoded)) {
-        return false
-    }
+	if strings.HasPrefix("!", encoded) {
+		return false
+	}
 
-    algorithm := hashAlgorithm(encoded)
+	algorithm := hashAlgorithm(encoded)
 
-    switch algorithm {
-        case
-            "argon2",
-            "bcrypt",
-            "bcrypt_sha256",
-            "crypt",
-            "md5",
-            "pbkdf2_sha1",
-            "pbkdf2_sha256",
-            "sha1",
-            "unsalted_md5",
-            "unsalted_sha1":
-            return true
-    }
+	switch algorithm {
+	case
+		"argon2",
+		"bcrypt",
+		"bcrypt_sha256",
+		"crypt",
+		"md5",
+		"pbkdf2_sha1",
+		"pbkdf2_sha256",
+		"sha1",
+		"unsalted_md5",
+		"unsalted_sha1":
+		return true
+	}
 
-    return false
+	return false
 }
 
 // CheckPassword validate if the raw password matches the encoded digest.
 // This is a shortcut that discovers the algorithm used in the encoded digest
 // to perform the correct validation.
 func CheckPassword(password string, encoded string) (bool, error) {
-    if (!IsPasswordUsable(encoded)) {
-        return false, nil
-    }
+	if !IsPasswordUsable(encoded) {
+		return false, nil
+	}
 
-    algorithm := hashAlgorithm(encoded)
+	algorithm := hashAlgorithm(encoded)
 
-    switch algorithm {
-        case "pbkdf2_sha256":
-            return VerifyPBKDF2SHA256(password, encoded), nil
-        case "pbkdf2_sha1":
-            return VerifyPBKDF2SHA1(password, encoded), nil
-        case
-            "argon2",
-            "bcrypt",
-            "bcrypt_sha256",
-            "crypt",
-            "md5",
-            "sha1",
-            "unsalted_md5",
-            "unsalted_sha1":
-            return false, fmt.Errorf("unchained: hasher not implemented %s", algorithm)
-    }
+	switch algorithm {
+	case "pbkdf2_sha256":
+		return VerifyPBKDF2SHA256(password, encoded), nil
+	case "pbkdf2_sha1":
+		return VerifyPBKDF2SHA1(password, encoded), nil
+	case
+		"argon2",
+		"bcrypt",
+		"bcrypt_sha256",
+		"crypt",
+		"md5",
+		"sha1",
+		"unsalted_md5",
+		"unsalted_sha1":
+		return false, fmt.Errorf("unchained: hasher not implemented %s", algorithm)
+	}
 
-    return false, fmt.Errorf("unchained: invaid hasher %s", algorithm)
+	return false, fmt.Errorf("unchained: invaid hasher %s", algorithm)
 }
--- unchained_test.go
+++ unchained_test.go
@@ -3,57 +3,57 @@ package unchained
 import "testing"
 
 func TestCheckPasswordPBKDF2SHA256WithValidPassword(t *testing.T) {
-    valid, err := CheckPassword("admin", "pbkdf2_sha256$24000$JMO9TJawIXB1$5iz40fwwc+QW6lZY+TuNciua3YVMV3GXdgkhXrcvWag=")
+	valid, err := CheckPassword("admin", "pbkdf2_sha256$24000$JMO9TJawIXB1$5iz40fwwc+QW6lZY+TuNciua3YVMV3GXdgkhXrcvWag=")
 
-    if (err != nil) {
-        t.Fatal(err)
-    } else if (!valid) {
-        t.Fatal("Password should be valid.")
-    }
+	if err != nil {
+		t.Fatal(err)
+	} else if !valid {
+		t.Fatal("Password should be valid.")
+	}
 }
 
 func TestCheckPasswordPBKDF2SHA256WithInvalidPassword(t *testing.T) {
-    valid, err := CheckPassword("wrongpassword", "pbkdf2_sha256$24000$JMO9TJawIXB1$5iz40fwwc+QW6lZY+TuNciua3YVMV3GXdgkhXrcvWag=")
+	valid, err := CheckPassword("wrongpassword", "pbkdf2_sha256$24000$JMO9TJawIXB1$5iz40fwwc+QW6lZY+TuNciua3YVMV3GXdgkhXrcvWag=")
 
-    if (err != nil) {
-        t.Fatal(err)
-    } else if (valid) {
-        t.Fatal("Password should be invalid.")
-    }
+	if err != nil {
+		t.Fatal(err)
+	} else if valid {
+		t.Fatal("Password should be invalid.")
+	}
 }
 
 func TestCheckPasswordPBKDF2SHA1WithValidPassword(t *testing.T) {
-    valid, err := CheckPassword("test", "pbkdf2_sha1$24000$zX573SspyROA$eqWjJBui5kY/TRXg2TwvSwA+2wk=")
+	valid, err := CheckPassword("test", "pbkdf2_sha1$24000$zX573SspyROA$eqWjJBui5kY/TRXg2TwvSwA+2wk=")
 
-    if (err != nil) {
-        t.Fatal(err)
-    } else if (!valid) {
-        t.Fatal("Password should be valid.")
-    }
+	if err != nil {
+		t.Fatal(err)
+	} else if !valid {
+		t.Fatal("Password should be valid.")
+	}
 }
 
 func TestCheckPasswordPBKDF2SHA1WithInvalidPassword(t *testing.T) {
-    valid, err := CheckPassword("wrongpassword", "pbkdf2_sha1$24000$zX573SspyROA$eqWjJBui5kY/TRXg2TwvSwA+2wk=")
+	valid, err := CheckPassword("wrongpassword", "pbkdf2_sha1$24000$zX573SspyROA$eqWjJBui5kY/TRXg2TwvSwA+2wk=")
 
-    if (err != nil) {
-        t.Fatal(err)
-    } else if (valid) {
-        t.Fatal("Password should be invalid.")
-    }
+	if err != nil {
+		t.Fatal(err)
+	} else if valid {
+		t.Fatal("Password should be invalid.")
+	}
 }
 
 func TestIsPasswordUsableWithValidPassword(t *testing.T) {
-    usabe := IsPasswordUsable("pbkdf2_sha256$24000$JMO9TJawIXB1$5iz40fwwc+QW6lZY+TuNciua3YVMV3GXdgkhXrcvWag=")
+	usabe := IsPasswordUsable("pbkdf2_sha256$24000$JMO9TJawIXB1$5iz40fwwc+QW6lZY+TuNciua3YVMV3GXdgkhXrcvWag=")
 
-    if (!usabe) {
-        t.Fatal("Password should be usable.")
-    }
+	if !usabe {
+		t.Fatal("Password should be usable.")
+	}
 }
 
 func TestIsPasswordUsableWithUnusablePassword(t *testing.T) {
-    usabe := IsPasswordUsable("!")
+	usabe := IsPasswordUsable("!")
 
-    if (usabe) {
-        t.Fatal("Password should be unusable.")
-    }
+	if usabe {
+		t.Fatal("Password should be unusable.")
+	}
 }
