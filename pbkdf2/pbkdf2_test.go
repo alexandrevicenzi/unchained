db49e7a01702f36e58f70e56ec10ed494e74eb4b
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2019-02-08 22:46:59 +0100

    Remove testify dependency.

--- pbkdf2/pbkdf2_test.go
+++ pbkdf2/pbkdf2_test.go
@@ -2,114 +2,236 @@ package pbkdf2
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestPBKDF2SHA1Encode1(t *testing.T) {
 	encoded, err := NewPBKDF2SHA1Hasher().Encode("admin", "1TMOT0Rohg3g", 120000)
-	assert.Nil(t, err)
-	assert.Equal(t, encoded, "pbkdf2_sha1$120000$1TMOT0Rohg3g$zVJ4+gcRcano9Qks+kcsgKeRnVs=")
+
+	if err != nil {
+		t.Fatalf("Encode error: %s", err)
+	}
+
+	expected := "pbkdf2_sha1$120000$1TMOT0Rohg3g$zVJ4+gcRcano9Qks+kcsgKeRnVs="
+
+	if encoded != expected {
+		t.Fatalf("Encoded hash %s does not match %s.", encoded, expected)
+	}
 }
 
 func TestPBKDF2SHA1Encode2(t *testing.T) {
 	encoded, err := NewPBKDF2SHA1Hasher().Encode("this-is-my-password", "G8rkK8UFRZWr", 80000)
-	assert.Nil(t, err)
-	assert.Equal(t, encoded, "pbkdf2_sha1$80000$G8rkK8UFRZWr$/UGcDmP7BCJDdBMTNVN5fG8Ty1g=")
+
+	if err != nil {
+		t.Fatalf("Encode error: %s", err)
+	}
+
+	expected := "pbkdf2_sha1$80000$G8rkK8UFRZWr$/UGcDmP7BCJDdBMTNVN5fG8Ty1g="
+
+	if encoded != expected {
+		t.Fatalf("Encoded hash %s does not match %s.", encoded, expected)
+	}
 }
 
 func TestPBKDF2SHA1Encode3(t *testing.T) {
 	encoded, err := NewPBKDF2SHA1Hasher().Encode("Th1S1sMYp4ssw0rd", "jkHRJ7pu8k0v", 120000)
-	assert.Nil(t, err)
-	assert.Equal(t, encoded, "pbkdf2_sha1$120000$jkHRJ7pu8k0v$bXzu5MnzrIHkCR76ramj/z9DTKY=")
+
+	if err != nil {
+		t.Fatalf("Encode error: %s", err)
+	}
+
+	expected := "pbkdf2_sha1$120000$jkHRJ7pu8k0v$bXzu5MnzrIHkCR76ramj/z9DTKY="
+
+	if encoded != expected {
+		t.Fatalf("Encoded hash %s does not match %s.", encoded, expected)
+	}
 }
 
 func TestPBKDF2SHA1Encode4(t *testing.T) {
 	encoded, err := NewPBKDF2SHA1Hasher().Encode("this$is#my@PASSWORD", "1TMOT0Rohg3g", 180000)
-	assert.Nil(t, err)
-	assert.Equal(t, encoded, "pbkdf2_sha1$180000$1TMOT0Rohg3g$1OBUXq+UswNEbPkNKGnB2BzVW4g=")
+
+	if err != nil {
+		t.Fatalf("Encode error: %s", err)
+	}
+
+	expected := "pbkdf2_sha1$180000$1TMOT0Rohg3g$1OBUXq+UswNEbPkNKGnB2BzVW4g="
+
+	if encoded != expected {
+		t.Fatalf("Encoded hash %s does not match %s.", encoded, expected)
+	}
 }
 
 func TestPBKDF2SHA1Verify1(t *testing.T) {
 	valid, err := NewPBKDF2SHA1Hasher().Verify("admin", "pbkdf2_sha1$120000$1TMOT0Rohg3g$zVJ4+gcRcano9Qks+kcsgKeRnVs=")
-	assert.Nil(t, err)
-	assert.True(t, valid)
+
+	if err != nil {
+		t.Fatalf("Verify error: %s", err)
+	}
+
+	if !valid {
+		t.Fatal("Password should be valid.")
+	}
 }
 
 func TestPBKDF2SHA1Verify2(t *testing.T) {
 	valid, err := NewPBKDF2SHA1Hasher().Verify("this-is-my-password", "pbkdf2_sha1$80000$G8rkK8UFRZWr$/UGcDmP7BCJDdBMTNVN5fG8Ty1g=")
-	assert.Nil(t, err)
-	assert.True(t, valid)
+
+	if err != nil {
+		t.Fatalf("Verify error: %s", err)
+	}
+
+	if !valid {
+		t.Fatal("Password should be valid.")
+	}
 }
 
 func TestPBKDF2SHA1Verify3(t *testing.T) {
 	valid, err := NewPBKDF2SHA1Hasher().Verify("Th1S1sMYp4ssw0rd", "pbkdf2_sha1$120000$1TMOT0Rohg3g$KQkAqdJmqnZZM3aY5KbPDXS6aDo=")
-	assert.Nil(t, err)
-	assert.True(t, valid)
+
+	if err != nil {
+		t.Fatalf("Verify error: %s", err)
+	}
+
+	if !valid {
+		t.Fatal("Password should be valid.")
+	}
 }
 
 func TestPBKDF2SHA1Verify4(t *testing.T) {
 	valid, err := NewPBKDF2SHA1Hasher().Verify("this$is#my@PASSWORD", "pbkdf2_sha1$180000$1TMOT0Rohg3g$1OBUXq+UswNEbPkNKGnB2BzVW4g=")
-	assert.Nil(t, err)
-	assert.True(t, valid)
+
+	if err != nil {
+		t.Fatalf("Verify error: %s", err)
+	}
+
+	if !valid {
+		t.Fatal("Password should be valid.")
+	}
 }
 
 func TestPBKDF2SHA1VerifyInvalidPassword(t *testing.T) {
 	valid, err := NewPBKDF2SHA1Hasher().Verify("wrongpassword", "pbkdf2_sha1$120000$1TMOT0Rohg3g$zVJ4+gcRcano9Qks+kcsgKeRnVs=")
-	assert.Nil(t, err)
-	assert.False(t, valid)
+
+	if err != nil {
+		t.Fatalf("Verify error: %s", err)
+	}
+
+	if valid {
+		t.Fatal("Password should not be valid.")
+	}
 }
 
 func TestPBKDF2SHA256Encode1(t *testing.T) {
 	encoded, err := NewPBKDF2SHA256Hasher().Encode("admin", "WZrFZhpl3wOU", 120000)
-	assert.Nil(t, err)
-	assert.Equal(t, encoded, "pbkdf2_sha256$120000$WZrFZhpl3wOU$yPimyWN658IuAu0XErvg1Nowfd55k60hu4o+eDUlBDM=")
+
+	if err != nil {
+		t.Fatalf("Encode error: %s", err)
+	}
+
+	expected := "pbkdf2_sha256$120000$WZrFZhpl3wOU$yPimyWN658IuAu0XErvg1Nowfd55k60hu4o+eDUlBDM="
+
+	if encoded != expected {
+		t.Fatalf("Encoded hash %s does not match %s.", encoded, expected)
+	}
 }
 
 func TestPBKDF2SHA256Encode2(t *testing.T) {
 	encoded, err := NewPBKDF2SHA256Hasher().Encode("this-is-my-password", "ITqksnfwCKZr", 80000)
-	assert.Nil(t, err)
-	assert.Equal(t, encoded, "pbkdf2_sha256$80000$ITqksnfwCKZr$P5PvQJSPR/dPZFdLDAiWlcEmQ5jyN7CPohEc5eIqNhE=")
+
+	if err != nil {
+		t.Fatalf("Encode error: %s", err)
+	}
+
+	expected := "pbkdf2_sha256$80000$ITqksnfwCKZr$P5PvQJSPR/dPZFdLDAiWlcEmQ5jyN7CPohEc5eIqNhE="
+
+	if encoded != expected {
+		t.Fatalf("Encoded hash %s does not match %s.", encoded, expected)
+	}
 }
 
 func TestPBKDF2SHA256Encode3(t *testing.T) {
 	encoded, err := NewPBKDF2SHA256Hasher().Encode("Th1S1sMYp4ssw0rd", "vM98pB74e18T", 120000)
-	assert.Nil(t, err)
-	assert.Equal(t, encoded, "pbkdf2_sha256$120000$vM98pB74e18T$WkDU2oo5q/qv7iCnZMmxLQWqX4QFrgSrhISfoe/+x4U=")
+
+	if err != nil {
+		t.Fatalf("Encode error: %s", err)
+	}
+
+	expected := "pbkdf2_sha256$120000$vM98pB74e18T$WkDU2oo5q/qv7iCnZMmxLQWqX4QFrgSrhISfoe/+x4U="
+
+	if encoded != expected {
+		t.Fatalf("Encoded hash %s does not match %s.", encoded, expected)
+	}
 }
 
 func TestPBKDF2SHA256Encode4(t *testing.T) {
 	encoded, err := NewPBKDF2SHA256Hasher().Encode("this$is#my@PASSWORD", "WZrFZhpl3wOU", 180000)
-	assert.Nil(t, err)
-	assert.Equal(t, encoded, "pbkdf2_sha256$180000$WZrFZhpl3wOU$mvtqm3pn05FRFL5GlG0WnPTa/EFEgUlAWT5+1kozxGY=")
+
+	if err != nil {
+		t.Fatalf("Encode error: %s", err)
+	}
+
+	expected := "pbkdf2_sha256$180000$WZrFZhpl3wOU$mvtqm3pn05FRFL5GlG0WnPTa/EFEgUlAWT5+1kozxGY="
+
+	if encoded != expected {
+		t.Fatalf("Encoded hash %s does not match %s.", encoded, expected)
+	}
 }
 
 func TestPBKDF2SHA256Verify1(t *testing.T) {
 	valid, err := NewPBKDF2SHA256Hasher().Verify("admin", "pbkdf2_sha256$120000$WZrFZhpl3wOU$yPimyWN658IuAu0XErvg1Nowfd55k60hu4o+eDUlBDM=")
-	assert.Nil(t, err)
-	assert.True(t, valid)
+
+	if err != nil {
+		t.Fatalf("Verify error: %s", err)
+	}
+
+	if !valid {
+		t.Fatal("Password should be valid.")
+	}
 }
 
 func TestPBKDF2SHA256Verify2(t *testing.T) {
 	valid, err := NewPBKDF2SHA256Hasher().Verify("this-is-my-password", "pbkdf2_sha256$80000$ITqksnfwCKZr$P5PvQJSPR/dPZFdLDAiWlcEmQ5jyN7CPohEc5eIqNhE=")
-	assert.Nil(t, err)
-	assert.True(t, valid)
+
+	if err != nil {
+		t.Fatalf("Verify error: %s", err)
+	}
+
+	if !valid {
+		t.Fatal("Password should be valid.")
+	}
 }
 
 func TestPBKDF2SHA256Verify3(t *testing.T) {
 	valid, err := NewPBKDF2SHA256Hasher().Verify("Th1S1sMYp4ssw0rd", "pbkdf2_sha256$120000$vM98pB74e18T$WkDU2oo5q/qv7iCnZMmxLQWqX4QFrgSrhISfoe/+x4U=")
-	assert.Nil(t, err)
-	assert.True(t, valid)
+
+	if err != nil {
+		t.Fatalf("Verify error: %s", err)
+	}
+
+	if !valid {
+		t.Fatal("Password should be valid.")
+	}
 }
 
 func TestPBKDF2SHA256Verify4(t *testing.T) {
 	valid, err := NewPBKDF2SHA256Hasher().Verify("this$is#my@PASSWORD", "pbkdf2_sha256$180000$WZrFZhpl3wOU$mvtqm3pn05FRFL5GlG0WnPTa/EFEgUlAWT5+1kozxGY=")
-	assert.Nil(t, err)
-	assert.True(t, valid)
+
+	if err != nil {
+		t.Fatalf("Verify error: %s", err)
+	}
+
+	if !valid {
+		t.Fatal("Password should be valid.")
+	}
 }
 
 func TestPBKDF2SHA256VerifyInvalidPassword(t *testing.T) {
 	valid, err := NewPBKDF2SHA256Hasher().Verify("wrongpassword", "pbkdf2_sha256$120000$WZrFZhpl3wOU$yPimyWN658IuAu0XErvg1Nowfd55k60hu4o+eDUlBDM=")
-	assert.Nil(t, err)
-	assert.False(t, valid)
+
+	if err != nil {
+		t.Fatalf("Verify error: %s", err)
+	}
+
+	if valid {
+		t.Fatal("Password should not be valid.")
+	}
 }
--- unchained_test.go
+++ unchained_test.go
@@ -2,68 +2,124 @@ package unchained
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestMakePasswordDefault(t *testing.T) {
-	hash, err := MakePassword("admin", "1TMOT0Rohg3g", "default")
-	assert.Nil(t, err)
-	assert.Equal(t, hash, "pbkdf2_sha256$180000$1TMOT0Rohg3g$yWmK6dJrGjnpIyllslJ9vlyiXP3WOGNQ74hjEofWJu4=")
+	encoded, err := MakePassword("admin", "1TMOT0Rohg3g", "default")
+
+	if err != nil {
+		t.Fatalf("MakePassword error: %s", err)
+	}
+
+	expected := "pbkdf2_sha256$180000$1TMOT0Rohg3g$yWmK6dJrGjnpIyllslJ9vlyiXP3WOGNQ74hjEofWJu4="
+
+	if encoded != expected {
+		t.Fatalf("Encoded hash %s does not match %s.", encoded, expected)
+	}
 }
 
 func TestMakePasswordEmptySaltDefault(t *testing.T) {
-	hash, err := MakePassword("admin", "", "default")
-	assert.Nil(t, err)
-	assert.Equal(t, IdentifyHasher(hash), PBKDF2SHA256Hasher)
+	encoded, err := MakePassword("admin", "", "default")
+
+	if err != nil {
+		t.Fatalf("MakePassword error: %s", err)
+	}
+
+	hasher := IdentifyHasher(encoded)
+
+	if hasher != DefaultHasher {
+		t.Fatalf("Hasher %s is not %s.", hasher, DefaultHasher)
+	}
 }
 
 func TestCheckPasswordArgon2(t *testing.T) {
 	valid, err := CheckPassword("admin", "argon2$argon2i$v=19$m=512,t=2,p=2$NnFZNGxmQTE1bmFV$kPPGrqD6dnRllcQeksFN+w")
-	assert.Nil(t, err)
-	assert.True(t, valid)
+
+	if err != nil {
+		t.Fatalf("CheckPassword error: %s", err)
+	}
+
+	if !valid {
+		t.Fatal("Password should be valid.")
+	}
 }
 
 func TestCheckPasswordPBKDF2SHA1(t *testing.T) {
 	valid, err := CheckPassword("admin", "pbkdf2_sha1$120000$1TMOT0Rohg3g$zVJ4+gcRcano9Qks+kcsgKeRnVs=")
-	assert.Nil(t, err)
-	assert.True(t, valid)
+
+	if err != nil {
+		t.Fatalf("CheckPassword error: %s", err)
+	}
+
+	if !valid {
+		t.Fatal("Password should be valid.")
+	}
 }
 
 func TestCheckPasswordPBKDF2SHA256(t *testing.T) {
 	valid, err := CheckPassword("admin", "pbkdf2_sha256$120000$WZrFZhpl3wOU$yPimyWN658IuAu0XErvg1Nowfd55k60hu4o+eDUlBDM=")
-	assert.Nil(t, err)
-	assert.True(t, valid)
+
+	if err != nil {
+		t.Fatalf("CheckPassword error: %s", err)
+	}
+
+	if !valid {
+		t.Fatal("Password should be valid.")
+	}
 }
 
 func TestCheckPasswordBCrypto(t *testing.T) {
 	valid, err := CheckPassword("admin", "bcrypt$$2b$12$qcNExitVe89wMG.nmRD4Qupn2hFm0pxvnu6VC.w6LShOx30l.F9/.")
-	assert.Nil(t, err)
-	assert.True(t, valid)
+
+	if err != nil {
+		t.Fatalf("CheckPassword error: %s", err)
+	}
+
+	if !valid {
+		t.Fatal("Password should be valid.")
+	}
 }
 
 func TestCheckPasswordBCryptoSHA256(t *testing.T) {
 	valid, err := CheckPassword("admin", "bcrypt_sha256$$2b$12$WZK9cb9qKN.Q5LCYPq/gj.6gvry1b37HUsJER6KhQBnIWmPyyaaqi")
-	assert.Nil(t, err)
-	assert.True(t, valid)
+
+	if err != nil {
+		t.Fatalf("CheckPassword error: %s", err)
+	}
+
+	if !valid {
+		t.Fatal("Password should be valid.")
+	}
 }
 
 func TestIsPasswordUsableWithValidPassword(t *testing.T) {
 	usable := IsPasswordUsable("pbkdf2_sha256$24000$JMO9TJawIXB1$5iz40fwwc+QW6lZY+TuNciua3YVMV3GXdgkhXrcvWag=")
-	assert.True(t, usable)
+
+	if !usable {
+		t.Fatal("Password should be usable.")
+	}
 }
 
 func TestIsPasswordUsableWithUnusablePassword1(t *testing.T) {
 	usable := IsPasswordUsable("!")
-	assert.False(t, usable)
+
+	if usable {
+		t.Fatal("Password should be unusable.")
+	}
 }
 
 func TestIsPasswordUsableWithUnusablePassword2(t *testing.T) {
 	usable := IsPasswordUsable("!password")
-	assert.False(t, usable)
+
+	if usable {
+		t.Fatal("Password should be unusable.")
+	}
 }
 
 func TestIsPasswordUsableWithEmptyPassword(t *testing.T) {
 	usable := IsPasswordUsable("")
-	assert.False(t, usable)
+
+	if usable {
+		t.Fatal("Password should be unusable.")
+	}
 }
